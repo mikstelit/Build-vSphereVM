a5980e40f9deaa8d1d593485a4b96886ada0d5c3
Mike.Littlefield@live.com <mike.littlefield@live.com>
2016-11-10 15:33:46 -0800

    Added specific error handling to Check-SCCMCollectionMembership to catch error thrown when sometimes updating All Systems collection.

--- Functions/Check-SCCMCollectionMembership.ps1
+++ Functions/Check-SCCMCollectionMembership.ps1
@@ -1,17 +1,26 @@
 ﻿Function Check-SCCMCollectionMembership($ComputerName, $Collection, $SiteCode, $SCCMServer)
 {
-    While($True)
+    Try
     {
-        $CollectionObject = Get-WmiObject -Namespace "Root\SMS\Site_$SiteCode" -Class SMS_Collection -Filter "Name = '$Collection'" -ComputerName $SCCMServer
-        $CollectionObject.RequestRefresh() | Out-Null
-
-        Start-Sleep 15
-
-        $CollectionMembers = Get-CMCollectionMember -CollectionName $Collection -ErrorAction Stop | foreach { $_.Name } | Sort-Object
-
-        If($CollectionMembers -and $CollectionMembers.Contains($ComputerName))
+        While($True)
         {
-            Break
+            $CollectionObject = Get-WmiObject -Namespace "Root\SMS\Site_$SiteCode" -Class SMS_Collection -Filter "Name = '$Collection'" -ComputerName $SCCMServer
+            $CollectionObject.RequestRefresh() | Out-Null
+
+            Start-Sleep 15
+
+            $CollectionMembers = Get-CMCollectionMember -CollectionName $Collection -ErrorAction Stop | foreach { $_.Name } | Sort-Object
+
+            If($CollectionMembers -and $CollectionMembers.Contains($ComputerName))
+            {
+                Break
+            }
         }
+    }
+    Catch
+    {
+        # Sometimes Get-CMCollectionMember throws an error when updating the All Systems collection.
+        # I have not found a solution for this, but am returning the function if the error occurs.
+        Return
     }
 }
